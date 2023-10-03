989790b3cbbad5c55382b164042c6e12ff46dbd8
venkatkp50 <95966437+venkatkp50@users.noreply.github.com>
2023-10-03 17:32:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -16,26 +16,41 @@ st.sidebar.title('Settings')
 with header:
     # st.image(IMAGE_PATH)  
     st.divider()
-    option1 = st.selectbox('Speaker',getSpeakerData())
-    option2 = st.selectbox('Files',getFileList(option1))
-    # st.write(FILE_TAXONOMY[option1])
-    df = getSpeakerLikelihood(option2)
-    speakerIdx = np.argmax(df['likelihood'])
-    predictedSpeaker = df.loc[speakerIdx][0]
+    st.header('Input')
+    predictedSpeaker = ''
+    speakerScore = ''
+    fileOption = st.radio('Choose File to Test',['From Folder', 'From Data File'])
+    
+    if fileOption == 'From Data File':
+        flag ='data_file'
+        option1 = st.selectbox('Speaker',getSpeakerData())
+        option2 = st.selectbox('Files',getFileList(option1))
+        # st.write(FILE_TAXONOMY[option1])
+        df = getSpeakerLikelihood(option2)
+        speakerIdx = np.argmax(df['likelihood'])
+        predictedSpeaker = df.loc[speakerIdx][0]
+        speakerScore = df.loc[speakerIdx][1]
+    else:
+        flag = 'browse'
+        uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=False,type=(["wav"]))
+        if uploaded_files is not None:
+            with open(os.path.join(temp_dir_path, uploaded_files.name), "wb") as f:
+                f.write(uploaded_files.read())
+            df = getSpeakerLikelihood(os.path.join(temp_dir_path, uploaded_files.name))
+            speakerIdx = np.argmax(df['likelihood'])
+            predictedSpeaker = df.loc[speakerIdx][0]
+            speakerScore = df.loc[speakerIdx][1]
 
-    col1, col2, col3, col4 = st.columns(4,gap='small')
+    st.divider()
+    st.header('Result')
+    col1, col2,col3 = st.columns(3,gap='small')
     with col1:
         st.error('Model')
         st.markdown('GMM')
     with col2:
-        st.error('Wave File')
-        st.markdown(option2)
-    with col3:
-        st.error('Expected Speaker')
-        st.markdown(option1)
-    with col4:
         st.error('Predicted Speaker')
         st.markdown(predictedSpeaker)
-   
-    
-
+    with col3:
+        st.error('Speaker Score')
+        st.markdown(speakerScore)
+        
